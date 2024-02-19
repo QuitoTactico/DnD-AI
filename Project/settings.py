172cb9d8661d20072cced76b064624c40c7f9e8b
MiguelCock <macockc@eafit.edu.co>
2024-02-18 20:53:45 -0500

    Improved the home page adding the basic template for the game

--- Project/settings.py
+++ Project/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
