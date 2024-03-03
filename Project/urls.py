b800d8a4c1a0eb0f6684dc5f11e846407d668159
QuitoTactico <teteban0917@gmail.com>
2024-03-03 18:42:35 -0500

    Models modified. Handles images and icons, and added their defaults.

--- Project/urls.py
+++ Project/urls.py
@@ -18,7 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from DnD_AI import views as dnd
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dnd.home)
 ]
+
+urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
