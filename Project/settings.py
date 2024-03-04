1f5f611ba5f4e39e0e197d385c27de03a930d224
QuitoTactico <teteban0917@gmail.com>
2024-03-03 23:52:24 -0500

    Added cool way to visualize the models of a Django project.

--- Project/settings.py
+++ Project/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DnD_AI',
+    'django_extensions',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
 ]
 
 MIDDLEWARE = [
