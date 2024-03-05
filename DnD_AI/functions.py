1a5b16bc100cdf6d07329569cbd780da8b495907
QuitoTactico <teteban0917@gmail.com>
2024-03-04 22:24:04 -0500

    creating default weapons stats

--- DnD_AI/functions.py
+++ DnD_AI/functions.py
@@ -1,5 +1,19 @@
+from .models import *
 from random import randint
+from .default import *
 
-# I put it here if it's needed to be used in another view or code section
 def roll_dice(): 
     return randint(1, 20)
+
+def create_every_template_weapon():
+    for weapon in DEFAULT_WEAPON_STATS:
+        if not Weapon.objects.filter(name=weapon).exists():
+            Weapon.objects.create(name=weapon, 
+                                  is_template=True, 
+                                  damage=DEFAULT_WEAPON_STATS[weapon]['damage'], 
+                                  range=DEFAULT_WEAPON_STATS[weapon]['range'], 
+                                  image=DEFAULT_WEAPON_STATS[weapon]['image'],
+                                  is_ranged=DEFAULT_WEAPON_STATS[weapon]['is_ranged'],
+                                  physical_description=DEFAULT_WEAPON_STATS[weapon]['physical_description'],
+                                  damage_type=DEFAULT_WEAPON_STATS[weapon]['damage_type'])
+            
