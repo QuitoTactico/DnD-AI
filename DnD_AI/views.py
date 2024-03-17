c1899327d589f48dd44ec6f6f3072fcd6ad0c167
QuitoTactico <teteban0917@gmail.com>
2024-03-17 18:13:44 -0500

    Embed map finished

--- DnD_AI/views.py
+++ DnD_AI/views.py
@@ -5,7 +5,11 @@ from .models import *
 from .functions import *
 
 from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource, Range1d
+from bokeh.models import Range1d
+from bokeh.embed import components
+
+from django.conf import settings
+import os
 
 # Create your views here.
 
@@ -54,9 +58,11 @@ def home(request):
         player = player_selection(player_name)
         monster = monster_selection_by_id(monster_id)
 
-        create_map(player)
-
         players = Character.objects.filter(is_playable=True)
+
+        host = request.get_host()
+        map = create_map(player, characters, monsters, host)
+        script, div = components(map)
 
         #test = request.GET.get('test')
         #test = request.POST['test']
@@ -72,13 +78,18 @@ def home(request):
                                       'characters':characters, 
                                       'weapons':weapons, 
                                       'dice_value': dice_value, 
-                                      'dice_needed': dice_needed} )
+                                      'dice_needed': dice_needed,
+                                      'script': script, 'div': div,
+                                      #'url_prueba' : os.path.join(settings.BASE_DIR, player.icon.url[1:]).replace('\\', '/')
+                                      'host': host,
+                                      'url_prueba' : host + player.icon.url
+                                      } )
     else:
         # If there's no get/post, then it selects the first playable character in the database
         player = player_selection(None)
         return render(request, 'home.html', {'player':player} )
     
-def create_map(player:Character):
+def create_map(player:Character, characters, monsters, host:str = None):
     '''
     #logo_src = ColumnDataSource(dict(url = player.icon.url))
     #logo_src = ColumnDataSource(player.icon.url)
@@ -98,14 +109,60 @@ def create_map(player:Character):
     show(map)
     '''
     #logo_src = ColumnDataSource(data=dict(url=['/..'+player.icon.url]))
-    map = figure(active_scroll='wheel_zoom', width=500, height=500, title="")
+    #icon_path = PurePath(player.icon.url)
+    #icon_path = icon_path.__str__()
+    #('stretch_width', 'stretch_height', 'stretch_both', 'scale_width', 'scale_height', 'scale_both', 'fixed', 'inherit')
+    map = figure(active_scroll='wheel_zoom', title="", aspect_scale=1, sizing_mode='scale_width', align='center')
     map.toolbar.logo = None
     map.toolbar_location = None
-    map.x_range = Range1d(start=0, end=1)
-    map.y_range = Range1d(start=0, end=1)
-    map.image_url(url=['http://localhost:8000/media/entity/icons/Frieren_icon.png'], x=0, y=0, h=1, w=1)
+    map.x_range = Range1d(start=(player.x)-2, end=(player.x)+3)
+    map.y_range = Range1d(start=(player.y)-2, end=(player.y)+3)
+    #map.x_range.start = (player.x)-2
+    #map.image_url(url=['http://127.0.0.1:8000'+player.icon.url], x=0, y=0, h=1, w=1)
+
+
+
+    for character in characters:
+        character_x, character_y = character.x, character.y
+        if host:
+            icon_path, weapon_path = character.icon.url, character.weapon.image.url
+        else:
+            icon_path, weapon_path = os.path.join(settings.BASE_DIR, character.icon.url[1:]).replace('\\', '/'), os.path.join(settings.BASE_DIR, character.weapon.image.url[1:]).replace('\\', '/')
+        map.image_url(url=[icon_path], x=character_x+0.1, y=character_y+0.9, h=0.8, w=0.8)
+        map.image_url(url=[weapon_path], x=character_x+0.5, y=character_y+0.5, h=0.4, w=0.4)
+        if player.is_playable:
+            map.rect(x=character_x+0.5, y=character_y+0.5, width=0.8, height=0.8, line_color="blue", fill_alpha=0, line_width=2)
+        else:
+            map.rect(x=character_x+0.5, y=character_y+0.5, width=0.8, height=0.8, line_color="yellow", fill_alpha=0, line_width=2)
+
+    for monster in monsters:
+        monster_x, monster_y = monster.x, monster.y
+        if host:
+            icon_path, weapon_path = monster.icon.url, monster.weapon.image.url
+        else:
+            icon_path, weapon_path = os.path.join(settings.BASE_DIR, monster.icon.url[1:]).replace('\\', '/'), os.path.join(settings.BASE_DIR, monster.weapon.image.url[1:]).replace('\\', '/')
+        map.image_url(url=[icon_path], x=monster_x+0.1, y=monster_y+0.9, h=0.8, w=0.8)
+        map.image_url(url=[weapon_path], x=monster_x+0.5, y=monster_y+0.5, h=0.4, w=0.4)
+        map.rect(x=monster_x+0.5, y=monster_y+0.5, width=0.8, height=0.8, line_color="red", fill_alpha=0, line_width=2)
+
+    # Player
+    player_x, player_y = player.x, player.y
+    if host:
+        icon_path, weapon_path = player.icon.url, player.weapon.image.url
+    else:
+        icon_path, weapon_path = os.path.join(settings.BASE_DIR, player.icon.url[1:]).replace('\\', '/'), os.path.join(settings.BASE_DIR, player.weapon.image.url[1:]).replace('\\', '/')
+    map.image_url(url=[icon_path], x=player_x+0.1, y=player_y+0.9, h=0.8, w=0.8)
+    map.image_url(url=[weapon_path], x=player_x+0.5, y=player_y+0.5, h=0.4, w=0.4)
+    map.rect(x=player_x+0.5, y=player_y+0.5, width=0.8, height=0.8, line_color="green", fill_alpha=0, line_width=2)
+ 
     map.outline_line_alpha = 0
-    show(map)
+    '''
+    map.aspect_scale = 1
+    map.sizing_mode = 'stretch_both'
+    map.aspect_scale = 1
+    '''
+    #show(map)
+    return map
 
 def player_selection(player_name):
     if player_name:
