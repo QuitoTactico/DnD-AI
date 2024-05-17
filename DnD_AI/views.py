b318cb010a7105f03bb1ace50dfff9f65d8a6bfc
QuitoTactico <teteban0917@gmail.com>
2024-05-17 00:12:45 -0500

    another bugfix, but this is the last, playercreation is perfect now

--- DnD_AI/views.py
+++ DnD_AI/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
 
 from .models import *
 from .functions import *
@@ -91,86 +92,93 @@ def playerSelection(request):
     - image         (optional) (default = icon)
     '''
 
-    
     if request.method == "POST":
 
         #campaign_id = request.POST['campaign_id'] if 'campaign_id' in request.POST else Campaign.objects.filter(is_completed=False).first().id  # REDUNDANT!
         # Today I've learnt something better than ternary conditional.
         campaign_id = request.POST.get('campaign_id') or Campaign.objects.filter(is_completed=False).first().id
 
-
-        # POR AHORA SIN CREACIÓN, AHORITA LO PROBAMOS
-        # This goes here. After the player selects his character info, the button will redirect the player to this view.
-        # So, the button will send the info to this view and not the other one, to create the character.
+        # Check if create_player is in the POST data
         if 'create_player' in request.POST:
-            name = request.POST.get('name')
-            physical_description = request.POST.get('physical_description')
+            #try:
+                name = request.POST.get('name')
+                physical_description = request.POST.get('physical_description')
 
-            character_race = request.POST.get('race')
-            character_class = request.POST.get('class') or character_race  # if there's no class selected, the race is used twice instead
+                character_race = request.POST.get('race')
+                character_class = request.POST.get('class') or character_race  # we use the race twice, if no class is selected
 
-            weapon_id = request.POST.get('weapon_id')
-            weapon = Weapon.objects.get(id=weapon_id)
+                weapon_id = int(request.POST.get('weapon_id'))
+                weapon = Weapon.objects.get(id=weapon_id)
 
-            max_health = request.POST.get('max_health') or request.POST.get('vitality')*10
-            strength = request.POST.get('str')
-            intelligence = request.POST.get('int')
-            #recursiveness = request.POST.get('rec')
-            dexterity = request.POST.get('dex')
-            physical_resistance = request.POST.get('phyres')
-            magical_resistance = request.POST.get('magres')
-            #constitution = request.POST.get('con')
+                # stats
+                max_health = int(request.POST.get('max_health', int(request.POST.get('vitality')) * 10))
+                strength = int(request.POST.get('str'))
+                intelligence = int(request.POST.get('int'))
+                dexterity = int(request.POST.get('dex'))
+                physical_resistance = int(request.POST.get('phyres'))
+                magical_resistance = int(request.POST.get('magres'))
 
-            story = request.POST.get('story') or "Default story."
+                story = request.POST.get('story') or "Default story."
 
-            new_player = Character.objects.create(
-                is_playable=True,
-                name=name,
-                physical_description=physical_description,
-                weapon=weapon,
-                max_health=max_health,
-                strength=strength,
-                intelligence=intelligence,
-                #recursiveness=recursiveness,
-                dexterity=dexterity,
-                physical_resistance=physical_resistance,
-                magical_resistance=magical_resistance,
-                #constitution=constitution,
-                inventory=str({'gold': 10, 'health potion': 5}),
-                story=story,
-                character_race=character_race,
-                character_class=character_class,
-                campaign_id=campaign_id,
-                x=0,
-                y=0,
-            )
+                '''
+                # we need to validate that numerical values do not exceed SQLite limits
+                if max_health > 9223372036854775807 or strength > 9223372036854775807 or intelligence > 9223372036854775807 or dexterity > 9223372036854775807 or physical_resistance > 9223372036854775807 or magical_resistance > 9223372036854775807:
+                    return HttpResponse("STATISTIC TOO BIG", status=400)
+                '''
 
-            place_player_on_spawn(new_player)
+                # create new player
+                new_player = Character.objects.create(
+                    is_playable=True,
+                    name=name,
+                    physical_description=physical_description,
+                    weapon=weapon,
+                    max_health=max_health,
+                    strength=strength,
+                    intelligence=intelligence,
+                    dexterity=dexterity,
+                    physical_resistance=physical_resistance,
+                    magical_resistance=magical_resistance,
+                    inventory=str({'gold': 10, 'health potion': 5}),
+                    story=story,
+                    character_race=character_race,
+                    character_class=character_class,
+                    campaign_id=campaign_id,
+                    x=0,
+                    y=0,
+                )
 
-            gift = request.POST.get('gift')
-            loot = {'gold': 50} if gift == 'gold' else {gift: 5}
-            new_player.add_all_to_inventory(loot)
+                place_player_on_spawn(new_player)
 
-            image_description = f"{new_player.name}, a {new_player.character_race} {new_player.character_class}, {new_player.physical_description}"
-            try:
-                image_dir_DallE = image_generator_DallE(image_description).replace('media/', '')
-                new_player.icon = image_dir_DallE
-                new_player.image = image_dir_DallE
-            except:
+                gift = request.POST.get('gift')
+                loot = {'gold': 50} if gift == 'gold' else {gift: 5}
+                new_player.add_all_to_inventory(loot)
+
+                image_description = f"{new_player.name}, a {new_player.character_race} {new_player.character_class}, {new_player.physical_description}"
                 try:
-                    image_dir_StabDiff = image_generator_StabDiff(image_description).replace('media/', '')
-                    new_player.icon = image_dir_StabDiff
-                    new_player.image = image_dir_StabDiff
+                    image_dir_DallE = image_generator_DallE(image_description).replace('media/', '')
+                    new_player.icon = image_dir_DallE
+                    new_player.image = image_dir_DallE
                 except:
-                    pass
+                    try:
+                        image_dir_StabDiff = image_generator_StabDiff(image_description).replace('media/', '')
+                        new_player.icon = image_dir_StabDiff
+                        new_player.image = image_dir_StabDiff
+                    except:
+                        pass
 
-            new_player.save()
-            # the player was created
-            
+                new_player.save()
+                '''
+                return HttpResponse("Player created successfully.")
+                
+            except OverflowError as e:
+                return HttpResponse(f"Overflow error: {e}", status=500)
+            except ValueError as e:
+                return HttpResponse(f"Value error: {e}", status=400)
+            '''
+
     else:
         campaign_id = Campaign.objects.filter(is_completed=False).first().id
-    
-    
+
     players = Character.objects.filter(campaign_id=campaign_id, is_playable=True).order_by('-level')
 
     return render(request, 
@@ -178,7 +186,8 @@ def playerSelection(request):
                   {
                       'players': players, 
                       'campaign_id': campaign_id
-                      })
+                  })
+
 
 
 
